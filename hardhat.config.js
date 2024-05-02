7f255f2534a48862e3931b4d6d67c29f7d364be0
Zulnabil Alhadad <zulnabil.22@gmail.com>
2024-05-02 15:46:43 +0700

    chore(network): change to testnet amoy

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,19 +2,19 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-require("dotenv").config();
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+require("@nomicfoundation/hardhat-toolbox")
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY } = process.env
 
 module.exports = {
   solidity: "0.8.9",
-  defaultNetwork: "polygon_mumbai",
+  defaultNetwork: "polygon_amoy",
   networks: {
     hardhat: {},
-    polygon_mumbai: {
+    polygon_amoy: {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
-};
+}
